1a326f9e3d36f179b18ee5fb9e8d8b322af1c794
Ben Osman <dev@smoothify.com>
2019-05-02 23:20:27 +0100

    Add bcrypt functionality to password resource

--- random/resource_password.go
+++ random/resource_password.go
@@ -1,7 +1,9 @@
 package random
 
 import (
+	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/terraform/helper/schema"
+	"golang.org/x/crypto/bcrypt"
 )
 
 /*********************************************************
@@ -14,7 +16,18 @@ import (
 func resourcePassword() *schema.Resource {
 	resourcePassword := resourceString()
 	resourcePassword.Create = CreatePassword
+	resourcePassword.Update = UpdatePassword
 	resourcePassword.Schema["result"].Sensitive = true
+	resourcePassword.Schema["bcrypt_cost"] = &schema.Schema{
+		Type:     schema.TypeInt,
+		Optional: true,
+		Default:  12,
+		ForceNew: false,
+	}
+	resourcePassword.Schema["bcrypt"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Computed: true,
+	}
 	return resourcePassword
 }
 
@@ -23,5 +36,24 @@ func CreatePassword(d *schema.ResourceData, meta interface{}) (err error) {
 		return err
 	}
 	d.SetId("none")
+	RepopulateHash(d, meta)
+	return nil
+}
+
+func UpdatePassword(d *schema.ResourceData, meta interface{}) (err error) {
+	RepopulateHash(d, meta)
+	return nil
+}
+
+func RepopulateHash(d *schema.ResourceData, _ interface{}) error {
+	bcryptCost := d.Get("bcrypt_cost").(int)
+	result := d.Get("result").(string)
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(result), bcryptCost)
+	if err != nil {
+		return errwrap.Wrapf("error generating bcrypt value: {{err}}", err)
+	}
+	d.Set("bcrypt", string(hash))
+
 	return nil
 }
